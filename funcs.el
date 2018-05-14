7916698cbd30c91a2b74c9428161f9889a2168cc
okamsn <ej32u@protonmail.com>
2018-05-13 20:27:36 -0400

    Initial upload.

---
+++ funcs.el
@@ -0,0 +1,172 @@
+;;; funcs.el --- Octave Layer functions File for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+
+;;;; functions for toggles
+
+(defun spacemacs/toggle-octave-echo-input ()
+  "Toggle whether input sent to Octave is echoed."
+  (interactive)
+  (if octave-send-echo-input
+      (progn
+        (setq octave-send-show-buffer nil)
+        (message "Won't echo submissions to Octave RELP."))
+    (progn
+      (setq octave-send-show-buffer t)
+      (message "Will echo submissions to Octave RELP."))))
+
+
+(defun spacemacs/toggle-octave-show-on-input ()
+  "Toggle whether sending input to Octave reveals its buffer."
+  (interactive)
+  (if octave-send-show-buffer
+      (progn
+        (setq octave-send-show-buffer nil)
+        (message "Won't show Octave RELP on send."))
+    (progn
+      (setq octave-send-show-buffer t)
+      (message "Will show Octave REPL on send."))))
+
+(defun spacemacs/toggle-octave-matlab-syntax-compatibility ()
+  "Toggle whether input sent to Octave is echoed."
+  (interactive)
+  (if spacemacs-octave-matlab-syntax-compatibility
+      (progn
+        (setq spacemacs-octave-matlab-syntax-compatibility
+              nil)
+        (message "Won't echo submissions to Octave RELP."))
+    (progn
+      (setq spacemacs-octave-matlab-syntax-compatibility
+            t)
+      (message "Will echo submissions to Octave RELP."))))
+
+;;;; functions to enable auto-complete
+(defun spacemacs//octave-switch-company-to-auto-complete ()
+  (if (bound-and-true-p company-mode) (company-mode -1))
+  (auto-complete-mode 1))
+
+
+;;;; repl controls
+
+(defun spacemacs/octave-switch-to-octave ()
+  "Switch buffer focus to Octave REPL"
+  (interactive)
+  (switch-to-buffer "*Inferior Octave*"))
+
+
+;;;; Functions to insert text
+
+(defun spacemacs/octave-insert-if ()
+  "Insert an 'if' block."
+  (interactive)
+  (end-of-line)
+  (newline)
+  (insert "if ")
+  (prog-indent-sexp)
+  (setq-local current-cursor-position
+              (point))
+  (newline)
+  (if spacemacs-octave-matlab-syntax-compatibility
+      (progn
+        (insert "end")
+        (prog-indent-sexp))
+    (smie-close-block))
+  (goto-char current-cursor-position)
+  (evil-append))
+
+
+(defun spacemacs/octave-insert-if-else ()
+  "Insert an 'if-else' block."
+  (interactive)
+  (end-of-line)
+  (newline)
+  (insert "if ")
+  (prog-indent-sexp)
+  (setq-local current-cursor-position
+              (point))
+  (newline)
+  (insert "else")
+  (prog-indent-sexp)
+  (newline)
+  (if spacemacs-octave-matlab-syntax-compatibility
+      (progn
+        (insert "end")
+        (prog-indent-sexp))
+    (smie-close-block))
+  (goto-char current-cursor-position)
+  (evil-append))
+
+(defun spacemacs/octave-insert-switch ()
+  "Insert an 'switch' block."
+  (interactive)
+  (end-of-line)
+  (newline)
+  (insert "switch ")
+  (prog-indent-sexp)
+  (setq-local current-cursor-position
+              (point))
+  (newline)
+  (insert "otherwise")
+  (prog-indent-sexp)
+  (newline)
+  (if spacemacs-octave-matlab-syntax-compatibility
+      (progn
+        (insert "end")
+        (prog-indent-sexp))
+    (smie-close-block))
+  (goto-char current-cursor-position)
+  (evil-append))
+
+(defun spacemacs/octave-insert-case ()
+  "Insert a 'case' stament."
+  (interactive)
+  (end-of-line)
+  (newline)
+  (insert "case ")
+  (prog-indent-sexp)
+  (evil-append))
+
+(defun spacemacs/octave-insert-for ()
+  "Insert a 'for' block."
+  (interactive)
+  (end-of-line)
+  (newline)
+  (insert "for ")
+  (prog-indent-sexp)
+  (setq-local current-cursor-position
+              (point))
+  (insert " = ")
+  (newline)
+  (if spacemacs-octave-matlab-syntax-compatibility
+      (progn
+        (insert "end")
+        (prog-indent-sexp))
+    (smie-close-block))
+  (goto-char current-cursor-position)
+  (evil-append))
+
+(defun spacemacs/octave-insert-while ()
+  "Insert a 'while' block."
+  (interactive)
+  (end-of-line)
+  (newline)
+  (insert "while ")
+  (prog-indent-sexp)
+  (setq-local current-cursor-position
+              (point))
+  (newline)
+  (if spacemacs-octave-matlab-syntax-compatibility
+      (progn
+        (insert "end")
+        (prog-indent-sexp))
+    (smie-close-block))
+  (goto-char current-cursor-position)
+  (evil-append))
---
+++ packages.el
@@ -0,0 +1,106 @@
+;;; packages.el --- Octave Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq octave-packages
+      '(auto-complete
+        ac-capf
+        ac-helm
+        ac-octave
+        ggtags
+        helm-gtags
+        (octave :location built-in)))
+
+(defun octave/init-octave ()
+  (use-package octave
+    :mode ("\\.m\\'" . octave-mode)
+    :commands (run-octave)
+    :init (progn
+            ;; (ac-octave-setup)
+            ;; (add-hook 'octave-mode-hook 'auto-complete-mode)
+            ;; (add-hook 'octave-mode-hook 'ac-octave-init)
+            (spacemacs/register-repl 'octave 'run-octave "octave"))
+    :config (progn
+              (spacemacs|defvar-company-backends octave-mode) ;; USE AC AS COMPANY BACKEND
+              ;; create default values for the toggles as needed
+              (setq-default spacemacs-octave-matlab-syntax-compatibility nil)
+
+             (spacemacs/declare-prefix-for-mode 'octave-mode "mb" "block controls")
+             (spacemacs/declare-prefix-for-mode 'octave-mode "mh" "help")
+             (spacemacs/declare-prefix-for-mode 'octave-mode "mi" "insert")
+             (spacemacs/declare-prefix-for-mode 'octave-mode "mg" "goto")
+             (spacemacs/declare-prefix-for-mode 'octave-mode "mm" "mark")
+             (spacemacs/declare-prefix-for-mode 'octave-mode "mr" "REPL controls")
+             (spacemacs/declare-prefix-for-mode 'octave-mode "ms" "submit/send")
+             (spacemacs/declare-prefix-for-mode 'octave-mode "mT" "toggle")
+             (spacemacs/set-leader-keys-for-major-mode 'octave-mode
+              ;; block controls
+              "bc" 'smie-close-block
+              "bm" 'octave-mark-block
+              ;; helpers
+              "hh" 'octave-help
+              "hi" 'octave-lookfor
+              ;; REPL
+              "'" 'run-octave
+              "sb" 'octave-send-buffer
+              "sp" 'octave-send-block
+              "sf" 'octave-send-defun
+              "si" 'run-octave
+              "sl" 'octave-send-line
+              "sr" 'octave-send-region
+              ;; gotos
+              "gD" 'octave-find-definition
+              "gn" 'octave-next-code-line
+              "gp" 'octave-previous-code-line
+              "ge" 'octave-end-of-line
+              "ga" 'octave-beginning-of-line
+              ;; insert
+              "iF" 'octave-insert-defun
+              "in" 'octave-indent-new-comment-line
+              "ii" 'spacemacs/octave-insert-if
+              "ie" 'spacemacs/octave-insert-if-else
+              "if" 'spacemacs/octave-insert-for
+              "is" 'spacemacs/octave-insert-switch
+              "ic" 'spacemacs/octave-insert-case
+              "iw" 'spacemacs/octave-insert-while
+              ;; REPL buffer controls
+              "rf" 'run-octave
+              "rs" 'octave-show-process-buffer
+              "rd" 'octave-hide-process-buffer
+              "rk" 'octave-kill-process
+              ;; search
+              ;; "sr" '
+              ;; Toggles
+              "Te" 'spacemacs/toggle-octave-echo-input
+              "Ts" 'spacemacs/toggle-octave-show-on-input
+              "Tm" 'spacemacs/toggle-octave-matlab-syntax-compatibility
+              ))))
+
+;; (defun octave/-init-ac)
+(defun octave/init-auto-complete ()
+  (ac-config-default))
+
+(defun octave/post-init-auto-complete ()
+  (add-hook 'octave-mode-hook 'spacemacs//octave-switch-company-to-auto-complete))
+
+(defun octave/init-ac-octave ()
+  (ac-octave-setup))
+
+(defun octave/post-init-ac-octave ()
+  (add-hook 'octave-mode-hook 'ac-octave-init))
+
+
+(defun octave/post-init-ggtags ()
+  "Make sure gtags are enabled, if available."
+  (add-hook 'octave-mode-local-vars-hook #'spacemacs/ggtags-mode-enable))
+
+(defun octave/post-init-helm-gtags ()
+  "Make sure gtags in Helm are enabled, if available."
+  (spacemacs/helm-gtags-define-keys-for-mode 'octave-mode))
